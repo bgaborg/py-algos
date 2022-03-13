b0545a9585267a4c31d18fbb0689b76011b266e5
Gabor Bota <gbota@cmtelematics.com>
2022-03-14 00:28:58 +0100

    [astar] refactor main a little bit

--- astar-py/astar_py/main.py
+++ astar-py/astar_py/main.py
@@ -61,6 +61,9 @@ class Display:
     def __init__(self, width):
         self.width = width
         self.window = None
+        self.grid = None
+        self.start = None
+        self.end = None
         window = pygame.display.set_mode((WIDTH, WIDTH))
         pygame.display.set_caption("A* Path Finding Algorithm Visualisation")
 
@@ -70,66 +73,67 @@ class Display:
 
     def main(self, window, width) -> None:
         ROWS = constants.ROWS
-        grid = make_grid(ROWS, width)
-
-        start = None
-        end = None
+        self.grid = make_grid(ROWS, width)
 
         run = True
         while run:
-            draw(window, grid, ROWS, width)
+            draw(window, self.grid, ROWS, width)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
             if pygame.mouse.get_pressed()[0]:  # LEFT
-                pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
-                spot = grid[row][col]
-                start, end = self.handle_left_click(start, end, spot)
+                spot = self.calculate_spot(width, ROWS)
+                self.handle_left_click(spot)
 
             elif pygame.mouse.get_pressed()[2]:  # RIGHT
-                pos = pygame.mouse.get_pos()
-                row, col = get_clicked_pos(pos, ROWS, width)
-                spot = grid[row][col]
-                start, end = self.handle_right_click(start, end, spot)
+                spot = self.calculate_spot(width, ROWS)
+                self.handle_right_click(spot)
 
             if event.type == pygame.KEYDOWN:
-                logger.debug(f"keydown: {event.key}")
-                if event.key == pygame.K_SPACE and start and end:
-                    for row in grid:
-                        for spot in row:
-                            spot.update_neighbors(grid)
-
-                    algorithm(lambda: draw(window, grid, ROWS, width), grid, start, end)
-
-                if event.key == pygame.K_BACKSPACE:
-                    start = None
-                    end = None
-                    grid = make_grid(ROWS, width)
+                self.handle_keydown(window, width, ROWS, self.grid, event)
 
         pygame.quit()
 
-    def handle_right_click(self, start, end, spot):
+    def calculate_spot(self, width, ROWS) -> Node:
+        pos = pygame.mouse.get_pos()
+        row, col = get_clicked_pos(pos, ROWS, width)
+        return self.grid[row][col]
+
+    def handle_keydown(self, window, width, ROWS, grid, event):
+        logger.debug(f"keydown: {event.key}")
+        if event.key == pygame.K_SPACE and self.start and self.end:
+            for row in grid:
+                for spot in row:
+                    spot.update_neighbors(grid)
+
+            algorithm(lambda: draw(window, grid, ROWS, width),self.grid, self.start, self.end)
+
+        if event.key == pygame.K_BACKSPACE:
+            self.start = None
+            self.end = None
+            self.grid = make_grid(ROWS, width)
+
+    def handle_right_click(self, spot):
         spot.reset()
-        if spot == start:
-            start = None
-        elif spot == end:
-            end = None
-        return start,end
+        if spot == self.start:
+            self.start = None
+        elif spot == self.end:
+            self.end = None
+        return self.start, self.end
 
-    def handle_left_click(self, start, end, spot):
-        if not start and spot != end:
-            start = spot
-            start.make_start()
+    def handle_left_click(self, spot):
+        if not self.start and spot != self.end:
+            self.start = spot
+            self.start.make_start()
 
-        elif not end and spot != start:
-            end = spot
-            end.make_end()
+        elif not self.end and spot != self.start:
+            self.end = spot
+            self.end.make_end()
 
-        elif spot != end and spot != start:
+        elif spot != self.end and spot != self.start:
             spot.make_barrier()
-        return start,end
+        return self.start, self.end
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
